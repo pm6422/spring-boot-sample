b814c2bce9f403bfc684d21fb13b7c6b20c38c33
louis <pm6422@126.com>
2024-05-23 19:21:47 +0800

    refactor

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client"
-import 'Frontend/src/index.css'
-import Main from './src/main'
-import { createElement } from 'react'
+import "Frontend/src/index.css"
+import Main from "./src/main"
+import { createElement } from "react"
 
-createRoot(document.getElementById('root')!).render(createElement(Main));
+createRoot(document.getElementById("root")!).render(createElement(Main))
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { RouterProvider } from 'react-router-dom'
-import { Toaster } from 'Frontend/src/components/ui/toaster'
-import { ThemeProvider } from 'Frontend/src/components/theme-provider'
-import router from 'Frontend/src/router'
-import 'Frontend/src/index.css'
+import React from "react"
+import { RouterProvider } from "react-router-dom"
+import { Toaster } from "Frontend/src/components/ui/toaster"
+import { ThemeProvider } from "Frontend/src/components/theme-provider"
+import router from "Frontend/src/router"
+import "Frontend/src/index.css"
 
 export default function Main() {
     return <React.StrictMode>
-                <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+                <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
                     <RouterProvider router={router} />
                     <Toaster />
                 </ThemeProvider>
