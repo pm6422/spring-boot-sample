bc70cd88ec22fdc22fe705df44ebfda6b8a86322
louis <pm6422@126.com>
2024-05-23 14:50:21 +0800

    refactor

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import 'Frontend/src/index.css'
-import App from './src/App.js'
+import App from './src/app.js'
 import { createElement } from 'react'
 
 ReactDOM.createRoot(document.getElementById('outlet')!).render(createElement(App));
