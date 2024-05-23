a4027b7f8cdb3a968654f886fe4ca33a9ba66083
louis <pm6422@126.com>
2024-05-23 11:28:15 +0800

    refactor

--- src/main/java/cn/luixtech/springbootsample/SpringBootSampleApplication.java
+++ src/main/java/cn/luixtech/springbootsample/SpringBootSampleApplication.java
@@ -2,14 +2,12 @@ package cn.luixtech.springbootsample;
 
 import com.luixtech.springbootframework.EnableLuixSpringBootFramework;
 import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 @EnableLuixSpringBootFramework
-//@Theme(value = "spring-boot-sample")
 public class SpringBootSampleApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
