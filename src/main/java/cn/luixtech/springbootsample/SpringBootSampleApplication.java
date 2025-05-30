5a3347e5c781a007c7568b35507f26b9d68004e4
pm6422 <pm6422@126.com>
2025-05-30 11:01:32 +0200

    refactor

--- src/main/java/cn/luixtech/springbootsample/SpringBootSampleApplication.java
+++ src/main/java/cn/luixtech/springbootsample/SpringBootSampleApplication.java
@@ -5,7 +5,6 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 @EnableLuixSpringBootFramework
 public class SpringBootSampleApplication implements AppShellConfigurator {
